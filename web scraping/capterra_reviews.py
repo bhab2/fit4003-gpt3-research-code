e086aa7be914d1681a69a370ef0c195823d01929
Vishal Jakhar <vjak0002@student.monash.edu>
2022-10-07 16:08:44 +1100

    fix review scraper

--- web scraping/capterra_reviews.py
+++ web scraping/capterra_reviews.py
@@ -35,7 +35,8 @@ try:
     for parent in correct_result.parents:
         if parent.name == 'a':
             searched_app_link = BASE_URL + parent.get('href')
-            searched_app_link = searched_app_link.replace('software', 'reviews')  # change link to reviews link
+            searched_app_link = searched_app_link.replace(
+                'software', 'reviews')  # change link to reviews link
 except AttributeError:
     print('The app could not be found on Capterra!')
     sys.exit(1)
@@ -49,32 +50,40 @@ with open(f'{fileName}.csv', 'w', encoding='UTF8') as f:
     # go through all pages of the website to get reviews
     for page in range(1, 100):
 
-        app_html_text = requests.get(searched_app_link + PAGE_URL + str(page)).text
+        app_html_text = requests.get(
+            searched_app_link + PAGE_URL + str(page)).text
         soup = BeautifulSoup(app_html_text, 'lxml')
         reviews = soup.find_all('div', class_='col-lg-7')
 
-        for index, review in enumerate(reviews):
-            reviewCount += 1
+        if len(reviews) == 0:
+            break
 
-            review_date = review.find('span', class_='ms-2').text.strip()  # gets the date of the review
-            # get the review title
-            review_title = review.find('h3', class_='h5 fw-bold').text.strip()
-            review_summary = None
-            review_comments = []
+        if reviews:
+            for index, review in enumerate(reviews):
+                reviewCount += 1
 
-            # go through all reviews
-            for p in review.find_all('p'):
+                # gets the date of the review
+                review_date = review.find('span', class_='ms-2').text.strip()
+                # get the review title
+                review_title = review.find(
+                    'h3', class_='h5 fw-bold').text.strip()
+                review_summary = None
+                review_comments = []
 
-                if p.find('span'):
-                    p.span.decompose()  # removes span tags within p tags
-                    review_summary = p.text.strip()
-                else:
-                    review_comments.append(p.text.strip())
+                # go through all reviews
+                for p in review.find_all('p'):
 
-            for comment in review_comments:
-                if comment.lower() == 'pros:' or comment.lower() == 'cons:':
-                    review_comments.remove(comment)
+                    if p.find('span'):
+                        p.span.decompose()  # removes span tags within p tags
+                        review_summary = p.text.strip()
+                    else:
+                        review_comments.append(p.text.strip())
 
-            writer.writerow([review_date, review_title, review_summary, review_comments])
+                for comment in review_comments:
+                    if comment.lower() == 'pros:' or comment.lower() == 'cons:':
+                        review_comments.remove(comment)
+
+                writer.writerow([review_date, review_title,
+                                review_summary, review_comments])
 
 print(f'{reviewCount} reviews to added csv file named {fileName}.csv')
